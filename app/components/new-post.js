210cb3e146fd7fb7c10f49d109ff65fea495e957
Asia Kane and Steven Galvin <steven@>
2017-05-31 16:43:02 -0700

    begin styling

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,20 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewPost: false,
   actions: {
-    postFormShow() {
-      this.set('addNewPost', true);
-    },
-
     savePost() {
       var params = {
-        title: this.get('title') ? this.get('title') : "",
-        date: this.get('date') ? this.get('date') : "",
-        image: this.get('image') ? this.get('image') : "get default image",
-        body: this.get('body') ? this.get('body') : "",
+        title: this.get('title') ? this.get('title') : "New Post",
+        date: this.get('date') ? this.get('date') : "N/A",
+        image: this.get('image') ? this.get('image') : "https://vetstreet.brightspotcdn.com/dims4/default/ff29db9/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fe1%2F36d0f0a8c311e0a0d50050568d634f%2Ffile%2FGoldendoodle-2-645mk070111.jpg",
+        body: this.get('body') ? this.get('body') : "How can you have a blog post with no body?",
       };
-      this.set('addNewPost', false);
       this.sendAction('savePost', params);
     }
   }
