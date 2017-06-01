24c929e332eba7beca6c6c7c1f2e607e5578943a
Asia Kane and Steven Galvin <steven@>
2017-06-01 14:48:12 -0700

    add defaults to empty comment fields.

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -8,9 +8,9 @@ export default Ember.Component.extend({
     },
     saveComment() {
       var params = {
-        author: this.get('author'),
-        date: this.get('date'),
-        content: this.get('content'),
+        author: this.get('author') ? this.get('author') : "Jesse Doe",
+        date: this.get('date') ? this.get('date') : "N/A",
+        content: this.get('content') ? this.get('content') : "N/A",
         post: this.get('post')
       };
       this.set('addNewComment', false);
